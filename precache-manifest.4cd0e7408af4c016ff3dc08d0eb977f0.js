ee69ecf89cddf09eba8117a56091dc7c2ba0f232
Samarth Ghulyani <g.samarth@outlook.com>
2020-08-04 14:48:45 +0530

    Updates

---
+++ precache-manifest.4cd0e7408af4c016ff3dc08d0eb977f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c30d6ef008402b3fd5fcd05f3378b0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "145c0f4bd52dcd3a8e42",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "de6124722d2dc1166799",
+    "url": "/static/js/2.8fd79ad1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8fd79ad1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "145c0f4bd52dcd3a8e42",
+    "url": "/static/js/main.8057e895.chunk.js"
+  },
+  {
+    "revision": "cf55e141549284d5416e",
+    "url": "/static/js/runtime-main.92733495.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
